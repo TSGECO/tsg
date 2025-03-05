398cdba215919c2a07c56777d353464d9e7652a3
Yangyang Li <yangyang.li@northwestern.edu>
2025-03-04 21:53:14 -0600

    feat: Update JSON export function to return Result and modify test data for edge types

--- src/io/json.rs
+++ src/io/json.rs
@@ -1,7 +1,8 @@
 use std::path::Path;
 
 use crate::graph::TSGraph;
+use anyhow::Result;
 
-pub fn to_json<P: AsRef<Path>>(tsg_graph: &TSGraph, output: P) -> String {
+pub fn to_json<P: AsRef<Path>>(tsg_graph: &TSGraph, output: P) -> Result<()> {
     todo!()
 }
